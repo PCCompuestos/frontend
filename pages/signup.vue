220eb09d68512afb96fa75cfe78ba3047e3c3729
Andres <andreees.ys@gmail.com>
2023-09-30 22:29:20 +0200

    conf

---
+++ pages/signup.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="w-full h-16 flex justify-center bg-white border-0 ring-1 ring-inset ring-gray-200 fixed z-10">
+    <div class="w-full max-w-4xl px-8 flex flex-row items-center justify-between">
+      <a href="/">PCCompuestos</a>
+      <div>
+        <a href="signup">Sign up</a>
+      </div>
+    </div>
+  </header>
+  <h1 class="pt-20">Sign Up</h1>
+</template>
