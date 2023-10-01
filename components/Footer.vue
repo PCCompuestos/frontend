3e6aecd637e7a814bac09a0d24e483f1babf04de
Andres <andreees.ys@gmail.com>
2023-10-01 17:04:58 +0200

    frontend

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,7 @@
 <template> 
-    <footer class="w-full h-16 flex justify-center bg-white border-0 ring-1 ring-inset ring-gray-200 fixed z-10"> 
-        Copyright PCCompuestos
-    </footer>
+  <footer class="w-full h-16 flex justify-center bg-white border-0 ring-1 ring-inset ring-gray-200 z-10"> 
+    <div class="w-full max-w-4xl px-8 flex flex-row items-center justify-between">
+      <p>©2023 PCCompuestos</p>
+    </div>
+  </footer>
 </template>
