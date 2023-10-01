2b0cfc45c36dce12912048396cd7f1777bf70a02
Andres <andreees.ys@gmail.com>
2023-10-01 16:43:01 +0200

    frontend

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,12 @@
 <template>
   <Header></Header>
-  <div class="w-full pt-20" style="background-image: url('../images/banner-gaming.jpg');">
-    <div class="w-full h-96"></div>
-    <!--<img src="../public/images/banner-gaming.jpg">-->
+  <!-- BANNER -->
+  <div class="w-full pt-20" style="background-image: url('../images/banner-gaming.jpg'); background-size: cover;">
+    <div class="w-full h-96 flex flex-col justify-center">
+      <h2 class="text-white text-center text-6xl font-bold">OFERTÓN 20%</h2>
+    </div>
   </div>
+  <!-- MOST SOLD -->
   <div class="h-20">
     <IconSearch class="h-20 w-20"/>
   </div>
