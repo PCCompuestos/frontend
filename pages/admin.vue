31f42d7a8b69d8962c5bc4c22c783504042bd8ac
Andres <andreees.ys@gmail.com>
2023-10-04 11:33:50 +0200

    html structure

--- pages/admin.vue
+++ pages/admin.vue
@@ -6,7 +6,7 @@
         <div class="pl-8 py-3 border-b cursor-pointer hover:bg-gray-100">Componentes</div>
         <div class="pl-8 py-3 border-b cursor-pointer hover:bg-gray-100">Fábrica</div>
         <div class="pl-8 py-3 border-b cursor-pointer hover:bg-gray-100">Pedidos</div>
-        <a href="settings" class="pl-8 py-3 border-b cursor-pointer hover:bg-gray-100">Usuarios</a>
+        <div class="pl-8 py-3 border-b cursor-pointer hover:bg-gray-100">Usuarios</div>
       </div>
       <div class="grow p-8 flex flex-row">
         <div class="h-52 w-52 mr-8 bg-gray-100"></div>
