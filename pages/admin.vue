9cb0dc2833363473a2161463674567092534daa9
Andres <andreees.ys@gmail.com>
2023-11-10 16:06:17 +0100

    token in header

--- pages/admin.vue
+++ pages/admin.vue
@@ -33,16 +33,16 @@
           <div class="p-2 border-solid border-2 border-black w-52">
             <label for="Name">Name:</label><br>
             <input type="text" id="Name" name="Name" class="border-solid border-2 border-black"
-            v-model="formData.Name"><br>
+            v-model="formData.name"><br>
             <label for="Description">Description:</label>
             <input type="text" id="Description" name="Description" class="border-solid border-2 border-black"
-            v-model="formData.Description"><br>
+            v-model="formData.description"><br>
             <label for="Quantity">Quantity:</label><br>
             <input type="text" id="Quantity" name="Quantity" class="border-solid border-2 border-black"
-            v-model="formData.Quantity"><br>
+            v-model="formData.quantity"><br>
             <label for="Price">Price:</label><br>
             <input type="text" id="Price" name="Price" class="border-solid border-2 border-black"
-            v-model="formData.Price"><br><br>
+            v-model="formData.price"><br><br>
             <button class="w-full bg-primary text-center cursor-pointer" @click="addProduct()">Añadir producto</button>
           </div><br>
           <p class="font-bold">LISTADO DE PRODUCTOS:</p>
@@ -76,8 +76,8 @@
           <p>Address: {{ user.address }}</p>
           <p>IsAdmin: {{ user.isadmin }}</p>
           <!-- Hacer que entre los dos botones haya una separación. No sé si es por la clase "card" -->
-          <button class="bg-primary cursor-pointer" @click="convertToAdmin(user)">Establecer administrador</button>
-          <button class="bg-primary cursor-pointer" @click="removeUser(user.id)">Eliminar usuario</button>
+          <Button @click="convertToAdmin(user)">Establecer administrador</Button>
+          <Button @click="removeUser(user.id)">Eliminar usuario</Button>
         </div>
       </div>
     </div>
@@ -86,19 +86,21 @@
 
 <style setup>
 .card {
-  @apply w-52 h-fit m-5;
+  @apply w-72 h-fit m-2 p-4 border shadow rounded-lg;
 }
 </style>
 
 <script setup>
+import { useUserStore } from "~/stores"
+const store = useUserStore()
+const token = store.token
 
 const formData = ref({
-  Name: '',
-  Description: '',
-  Quantity: '',
-  Price: ''
+  name: '',
+  description: '',
+  quantity: '',
+  pricerice: ''
 })
-
 
 const selected = ref('users')
 const { data: products } = await useFetch('http://localhost:3001/products')
@@ -110,6 +112,9 @@ const { data: users } = await useFetch('http://localhost:3001/users')
 async function convertToAdmin(user) {
   let result = await useFetch('http://localhost:3001/users/' + user.id, {
     method: 'put',
+    headers: {
+      'Authorization': `Bearer ${token}`
+    },
     body: {
       name: user.name,
       password: user.password,
@@ -127,11 +132,14 @@ async function convertToAdmin(user) {
 async function addProduct(){
   let result = await useFetch('http://localhost:3001/products', {
     method: 'post',
+    headers: {
+      'Authorization': `Bearer ${token}`
+    },
     body: {
-      name: formData.value.Name,
-      description: formData.value.Description,
-      quantity: formData.value.Quantity,
-      price: formData.value.Price
+      name: formData.value.name,
+      description: formData.value.description,
+      quantity: formData.value.quantity,
+      price: formData.value.price
     }
   })
   if (result.status._value == "success") {
@@ -141,21 +149,30 @@ async function addProduct(){
 
 async function removeUser(id) {
   await useFetch('http://localhost:3001/users/' + id, {
-    method: 'delete'
+    method: 'delete',
+    headers: {
+      'Authorization': `Bearer ${token}`
+    },
   })
   users.value = users.value.filter(user => user.id !== id)
 }
 
 async function removeOrder(id) {
   await useFetch('http://localhost:3001/orders/' + id, {
-    method: 'delete'
+    method: 'delete',
+    headers: {
+      'Authorization': `Bearer ${token}`
+    },
   })
   orders.value = orders.value.filter(order => order.id !== id);
 }
 
 async function removeProduct(id) {
   await useFetch('http://localhost:3001/products/' + id, {
-    method: 'delete'
+    method: 'delete',
+    headers: {
+      'Authorization': `Bearer ${token}`
+    },
   })
   products.value = products.value.filter(product => product.id !== id);
 }
@@ -163,7 +180,10 @@ async function removeProduct(id) {
 // No se gestiona que pueda estar incluida en alguna relación por lo que no se puede eliminar realmente
 async function removeComponent(code) {
   await useFetch('http://localhost:3001/components/' + code, {
-    method: 'delete'
+    method: 'delete',
+    headers: {
+      'Authorization': `Bearer ${token}`
+    },
   })
   components.value = components.value.filter(component => component.code !== code);
 }
