a76566b92b31f833c60e0404c0c4264d3ae7c597
Andres <andreees.ys@gmail.com>
2023-11-22 15:43:16 +0100

    admin middleware

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -2,13 +2,13 @@ import { useUserStore } from "~/stores"
 
 export default defineNuxtRouteMiddleware((to, from) => {
   const store = useUserStore()
-  const user = store.user
 
   if (!store.token) {
     return navigateTo('/login')
   }
+
   // Redirigir a la página de admin si el usuario tiene el valor isAdmin en true
-  /*if (user.isAdmin) {
+  if (to.name == "dashboard" && store.user.isAdmin) {
     return navigateTo('/admin')
-  }*/
+  }
 })
