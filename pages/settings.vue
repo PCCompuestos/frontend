09720e37d7f98a2a4fe0e75422b13c1754e28c2d
Javier Sin Pelayo <843442@unizar.es>
2023-11-23 12:26:25 +0100

    s

--- pages/settings.vue
+++ pages/settings.vue
@@ -6,13 +6,6 @@
       <div class="py-5 w-80 flex flex-col items-center">
         <div class="w-full justify-start">Cambio username</div>
         <input 
-          type="text" 
-          class="w-full p-2.5 my-1 border-0 ring-1 ring-inset ring-gray-200 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary text-gray-900 rounded-lg " 
-          placeholder="Nombre de usuario" 
-          required
-          v-model="formData1.name"
-        >
-        <input 
           type="password" 
           class=" w-full p-2.5 my-1 border-0 ring-1 ring-inset ring-gray-200 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary text-gray-900 rounded-lg" 
           placeholder="Contraseña" 
@@ -30,13 +23,6 @@
       </div>      
       <div class="py-5 w-80 flex flex-col items-center">
         <div class="w-full justify-start">Cambio contraseña</div>
-        <input 
-          type="text" 
-          class="w-full p-2.5 my-1 border-0 ring-1 ring-inset ring-gray-200 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary text-gray-900 rounded-lg " 
-          placeholder="Nombre de usuario" 
-          required
-          v-model="formData2.name"
-        >
         <input 
           type="password" 
           class=" w-full p-2.5 my-1 border-0 ring-1 ring-inset ring-gray-200 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary text-gray-900 rounded-lg" 
@@ -60,63 +46,90 @@
 <script setup>
 //FALTA IMPLEMENTAR QUE EL CAMBIO SE HAGA EFECTIVO EN LA BD
 
+import { useUserStore } from "~/stores"
+const store = useUserStore()
+const token = store.token
+const user = store.user
+const headers = {'Authorization': `Bearer ${token}`}
+
 const formData1 = ref({
-  name: '',
   password: '',
   newUserName: '',
 })
 
 const formData2 = ref({
-  name: '',
   password: '',
   newPassword: '',
 })
 
-const { data: users } = await useFetch('http://localhost:3001/users')
-let userIndex = 0;
 
-async function userExists(user) {
+// async function userExists(user) {
 
-  for (let i = 0; i < users._rawValue.length; i++) {
-    if (users._rawValue[i].name == user) {
-      userIndex = i;
-      // console.log(userIndex);
-      return true; // User with the given email exists
-    }
-  }
-  return false; // User does not exist
-}
+//   for (let i = 0; i < users._rawValue.length; i++) {
+//     if (users._rawValue[i].name == user) {
+//       userIndex = i;
+//       // console.log(userIndex);
+//       return true; // User with the given email exists
+//     }
+//   }
+//   return false; // User does not exist
+// }
 
 async function passwordMatches(password) {
-
-  if (users._rawValue[userIndex].password == password) {
-    return true; // Password matches
-  }
+  let result = await fetch('http://localhost:3001/users/' + user.id, {
+    method: 'get',
+    headers: headers,
+  })
+  console.log(result)
+  // if (password == result.password) {
+  //   return true; // Password matches
+  // }
 
   return false; // Password does not match
 }
 
+async function updateUsername(newUsername){
+  // console.log(users._rawValue[userIndex].id)
+  let result = await fetch('http://localhost:3001/users/' + user.id, {
+    method: 'put',
+    headers: headers,
+    body: {
+      name: newUsername,
+      password: user.password,
+      isadmin: user.isadmin,
+      email: user.email,
+      address: user.address
+    }
+  })
+  if(result.status._value == "success"){
+    alert(`¡Username changed successfully!`)
+    // alert(`Changing username from ${username} to ${newUsername}`)
+  } else {
+    alert('Error changing username')
+  }
+}
 
 // Function to handle the username change
 async function handleUsernameChange() {
-  const username = formData1.value.name;
   const password = formData1.value.password;
   const newUsername = formData1.value.newUserName;
 
+  // console.log(username);
+  // console.log(password);
+  // console.log(newUsername);
+
   // Check if the username exists and the password matches
-  if (await userExists(username)) {
-    if (await passwordMatches(password)) {
-      alert(`Changing username from ${username} to ${newUsername}`)
-      // console.log(`Changing username from ${username} to ${newUsername}`);
-      // return true;
-    } else {
-      // Display an alert for incorrect username or password
-      alert('Contraseña incorrecta. Por favor, verifica e intenta de nuevo.');
-    }
+  // if (await userExists(username)) {
+  if (await passwordMatches(password)) {
+    updateUsername(newUsername);
   } else {
-    // Display an alert for incorrect username 
-    alert('No existe ningún usuario con ese username.');
+    // Display an alert for incorrect username or password
+    alert('Contraseña incorrecta. Por favor, verifica e intenta de nuevo.');
   }
+  // } else {
+    // Display an alert for incorrect username 
+    // alert('No existe ningún usuario con ese username.');
+  // }
 }
 
 
