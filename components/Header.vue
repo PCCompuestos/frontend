fda3145d6a8eb2857206394991bbb976f186e1b7
Javier Sin Pelayo <843442@unizar.es>
2023-11-26 11:22:30 +0100

    cambios logo

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,9 @@
 <template>
   <header class="w-full h-16 flex justify-center bg-white border-0 ring-1 ring-inset ring-gray-200 fixed z-10">
     <div class="w-full px-8 flex flex-row items-center justify-between"  :class="{'max-w-4xl': !store.token}">
-      <a href="/" class="font-bold">PCCompuestos</a>
+      <a href="/">
+        <img src="../public/images/PCCompuestos.ico" alt="Logo de aplicación">
+      </a>
       <div class="flex flex-row items-center">
         <a v-if="!store.token" href="/login">Iniciar sesión</a>        
         <a v-if="store.token" href="/dashboard" class="pr-6"><IconUserCircle v-if="store.token" stroke-width="1.25" class="h-10 w-10"></IconUserCircle></a>
